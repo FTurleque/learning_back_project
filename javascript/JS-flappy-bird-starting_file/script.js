3e2c290d3443f04158c9e80588f934ba971200c1
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2021-10-02 13:16:55 +0200

    Début Jeux Flappy

---
+++ javascript/JS-flappy-bird-starting_file/script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContex('2d');
+const img = new Image();
+img.src = './media/flappy-bird-set.png';
+
+
+/**General settings */
