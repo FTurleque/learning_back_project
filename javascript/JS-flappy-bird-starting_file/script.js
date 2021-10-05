bee1a38fd1267747d47ead66f4dffa92dfd29c90
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-10-05 12:57:39 +0200

    Update script.js

--- javascript/JS-flappy-bird-starting_file/script.js
+++ javascript/JS-flappy-bird-starting_file/script.js
@@ -5,3 +5,25 @@ img.src = './media/flappy-bird-set.png';
 
 
 /**General settings */
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+    bestScore = 0,
+    currentScore = 0,
+    pipes= [],
+    flight,
+    flyHeight;
+
+const render = () => {
+    index++;
+
+    ctx.drawImage(img, 432,0, ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+
+    window.requestAnimationFrame(render);
+}
+img.onload = render;
