b14e69eaf1d80805de76caf6da6af10d561df23c
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2021-10-26 21:22:00 +0200

    tmdb_v3

--- javascript/stream/js/app.js
+++ javascript/stream/js/app.js
@@ -1,5 +1,5 @@
-import {getVideoMovieById, getMovieById, getMovieBackdrops, getMovieToDiscover, url_discover} from './tmdb.js'
-import {createDivWithClass, createAWithClass, createH2WithClass, createImgClass} from './constructor_html.js'
+import {getMovieToDiscover} from './tmdb.js'
+
 const sliderNewsMovies = document.querySelector('.dicover');
 const sliderAction = document.querySelector('.action');
 const sliderAnimation = document.querySelector('.animation');
@@ -20,22 +20,6 @@ const sliderAnimation = document.querySelector('.animation');
 // const sliderThriller = document.querySelector('.thriller');
 // const sliderWestren = document.querySelector('.westren');
 
-// Je récupère les données des films a découvrir
-let discoverMovie = await getMovieToDiscover(url_discover);
-console.log(discoverMovie);
 
-// J'extrait les données de chaque film à découvrir
-let idMoviesToDiscover = [];
-function extractData(_data) {
-    _data.results.forEach(movie => {
-        idMoviesToDiscover.push(movie.id);
-    });
-}
 
-extractData(discoverMovie)
-
-// getMovieBackdrops(580489);
-
-// let trailerMovie = getVideoMovieById(580489);
-
-// getMovieById(580489);
+getMovieToDiscover();
--- javascript/stream/js/constructor_html.js
+++ javascript/stream/js/constructor_html.js
@@ -1,26 +1,26 @@
 
-let createDivWithClass = function(className) {
-    let div = document.createElement('div');
-    div.setAttribute('class', className);
-    return div;
-}
+// let createDivWithClass = function(className) {
+//     let div = document.createElement('div');
+//     div.setAttribute('class', className);
+//     return div;
+// }
 
-let createAWithClass = function(className) {
-    let a = document.createElement('a');
-    a.setAttribute('class', className);
-    return a;
-}
+// let createAWithClass = function(className) {
+//     let a = document.createElement('a');
+//     a.setAttribute('class', className);
+//     return a;
+// }
 
-let createH2WithClass = function(className) {
-    let h2 = document.createElement('h2');
-    h2.setAttribute('class', className);
-    return h2;
-}
+// let createH1WithClass = function(className) {
+//     let h1 = document.createElement('h1');
+//     h1.setAttribute('class', className);
+//     return h1;
+// }
 
-let createImgClass = function(className) {
-    let img = document.createElement('img');
-    img.setAttribute('class', className);
-    return img;
-}
+// let createImgWithClass = function(className) {
+//     let img = document.createElement('img');
+//     img.setAttribute('class', className);
+//     return img;
+// }
 
-export {createDivWithClass, createAWithClass, createH2WithClass, createImgClass}
+// export {createDivWithClass, createAWithClass, createH1WithClass, createImgWithClass}
